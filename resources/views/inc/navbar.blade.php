1a676f200899238482b1b54512a1adaa0fcc3037
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-17 21:25:31 +0000

    fixed navbar to inlcude drop down admin menu

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -11,12 +11,37 @@
           <li class="{{Request::is('/photos') ? 'nav-item active' : '' }}">
             <a class="nav-link" href="/photos">Photos</a>
           </li>
+        </ul>
 
 
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+                <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+            @else
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
 
-
-
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
         </ul>
 
       </div>
     </nav>
+
+
+   
